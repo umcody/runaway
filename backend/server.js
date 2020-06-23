5866f4e214eaa87b8599220ef2c7abe7f9ab0741
codinggosu <leedongjoo96@naver.com>
2020-06-24 00:25:53 +0900

    fixed issue 4, dbconnect syntax error

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 7000
 const blogRoute = require("./route/blog.js");
 
 //connect to the database // for now, the password will be in the file
-mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://runaway-database:2gn5YAq0BRwLc7tF@runaway-mrvci.mongodb.net/<dbname>?retryWrites=true&w=majority", {useNewUrlParser=true});
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://runaway-database:2gn5YAq0BRwLc7tF@runaway-mrvci.mongodb.net/<dbname>?retryWrites=true&w=majority", {useNewUrlParser : true});
 
 app.get("/", function(req,res){
     return res.status(200).send("Hello World");
@@ -16,8 +16,6 @@ app.get("/", function(req,res){
 
 //Route function called
 blogRoute(app,mongoose);
-
-
 
 
 
