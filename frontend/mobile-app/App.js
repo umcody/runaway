3cc2e9ab537c1f45281170bd9aa7429f6f2c4580
Alex Chao <ac590@duke.edu>
2020-06-24 10:29:34 -0500

    fixed navigation?

--- frontend/mobile-app/App.js
+++ frontend/mobile-app/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-import Navigation from "./Navigation";
+import Navigation from "./Navigation/index";
 
 export default function App() {
   return <Navigation />;
