646b7e0af863fcdbfc85ccb0561d62c12790cdbf
Cody Um <yunsuum@gmail.com>
2020-06-24 10:23:18 +0900

    bug

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,6 @@ const PORT = process.env.PORT || 7000
 const blogRoute = require("./route/blog.js");
 
 //connect to the database // for now, the password will be in the file
-<<<<<<< HEAD
 mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://runaway-database:2gn5YAq0BRwLc7tF@runaway-mrvci.mongodb.net/<dbname>?retryWrites=true&w=majority", {useNewUrlParser: true});
 
 const db = mongoose.connection;
@@ -16,9 +15,6 @@ db.on("error", console.error.bind(console, "connection error"));
 db.once("open", () => {
     console.log("Connection succeeded");
 });
-=======
-mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://runaway-database:2gn5YAq0BRwLc7tF@runaway-mrvci.mongodb.net/<dbname>?retryWrites=true&w=majority", {useNewUrlParser:true});
->>>>>>> b8978901f249340e59c3b77ba709abc792a4dd1f
 
 app.get("/", function(req,res){
     return res.status(200).send("Hello World");
