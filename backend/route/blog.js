02229e1217c215fb9fbf91ec41cdcb87a2d1db5f
cedricblaise <cblaise@bowdoin.edu>
2020-06-24 23:21:57 -0400

    Bug fixes for GET blog request

--- backend/route/blog.js
+++ backend/route/blog.js
@@ -20,19 +20,20 @@ module.exports = function(app,mongoose){
     })
     
     // GET Blog posts, limit to 20 posts
-    app.get("api/volunteer/blog/get/:page", function(req, res) {
+    app.get("/api/volunteer/blog/get/:page", function(req, res) {
         const resPerPage = 20;  // results per page
         const page = req.params.page || 1; // Page
         
         blogModel.find(null, null, { skip: (resPerPage * (page - 1)), limit: resPerPage }, function(err, blogs) {
             if (err) throw new Error(err);
             const count = blogs.length;
+            console.log(count);
 
             // if there are no more blog posts, return no more found
-            if (count.length == 0) {
+            if (count == 0) {
                 res.send("No more blog posts");
             } else {
-                res.send(blogs);
+                res.json(blogs);
             }
         });
 
