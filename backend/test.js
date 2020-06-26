63741a9ac9f7cce638fe56c7942b010e608c25d2
cedricblaise <cblaise@bowdoin.edu>
2020-06-26 18:48:26 -0400

    Test for get blog endpoint

--- backend/test.js
+++ backend/test.js
@@ -15,3 +15,16 @@ describe("/GET Hello World", () => {
             })
     })
 })
+
+describe("/GET Blog Posts", () =>{
+    it("should return array of blog posts", (done) => {
+        chai.request(server)
+            .get("/api/volunteer/blog/get/1")
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a("array");
+                res.body.length.should.be.eql(5);
+                done();
+            });
+    });
+});
