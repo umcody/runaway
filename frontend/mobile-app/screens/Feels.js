912e3ee908a7a3f0e6cf833cccf1dcba46dcaadc
wamaue <wamaue@vt.edu>
2020-06-26 16:45:29 -0400

    feelings lists

--- frontend/mobile-app/screens/Feels.js
+++ frontend/mobile-app/screens/Feels.js
@@ -6,13 +6,51 @@ import {
   Text,
   TouchableOpacity,
   FlatList,
+  Dimensions,
 } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 
 export default function Chat({ navigation }) {
+  const feelingsCol1 = [
+    { title: "Happy", id: 1 },
+    { title: "Fearful", id: 2 },
+    { title: "Shameful", id: 3 },
+    { title: "Embarrassed", id: 4 },
+    { title: "Disgusted", id: 5 },
+  ];
+
+  const feelingsCol2 = [
+    { title: "Sad", id: 1 },
+    { title: "Angry", id: 2 },
+    { title: "Frustrated", id: 3 },
+    { title: "Stressed", id: 4 },
+    { title: "Surprised", id: 5 },
+  ];
+
+  const FeelingsList = ({ title }) => {
+    return (
+      <View>
+        <TouchableOpacity style={styles.feels}>
+          <Text>{title}</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  };
+
   return (
     <View style={styles.profile}>
-      <Text>This is the feelings screen screen</Text>
+      <View style={styles.col1}>
+        <FlatList
+          data={feelingsCol1}
+          renderItem={({ item }) => <FeelingsList title={item.title} />}
+        />
+      </View>
+      <View style={styles.col2}>
+        <FlatList
+          data={feelingsCol2}
+          renderItem={({ item }) => <FeelingsList title={item.title} />}
+        />
+      </View>
       <TouchableOpacity
         style={styles.buttonDismiss}
         onPress={() => {
@@ -24,6 +62,10 @@ export default function Chat({ navigation }) {
     </View>
   );
 }
+
+const windowW = Dimensions.get("window").width;
+const windowH = Dimensions.get("window").height;
+
 const styles = StyleSheet.create({
   home: {
     flex: 1,
@@ -31,10 +73,12 @@ const styles = StyleSheet.create({
   },
   profile: {
     flex: 1,
-    alignItems: "center",
     justifyContent: "center",
+    flexDirection: "row",
   },
   buttonDismiss: {
+    position: "absolute",
+    top: windowH - 125,
     borderRadius: 30,
     borderWidth: 2,
     backgroundColor: "#FF9EDA",
@@ -44,5 +88,24 @@ const styles = StyleSheet.create({
     width: 100,
     alignItems: "center",
     textAlign: "center",
+  },
+  col1: {
+    position: "absolute",
+    left: windowW / 4 - 63,
+  },
+  col2: {
+    position: "absolute",
+    left: 3 * (windowW / 4) - 63,
+  },
+  feels: {
+    fontSize: 20,
+    marginTop: 100,
+    borderColor: "#ACDAFF",
+    borderWidth: 2,
+    backgroundColor: "#E3F1FC",
+    alignItems: "center",
+    width: 126,
+    height: 126,
+    paddingTop: 50,
   },
 });
