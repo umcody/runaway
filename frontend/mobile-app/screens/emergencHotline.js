0bf0c3766a570ea115292231e0c5b18111d1a0c7
YasirAzizi <Yasir720@my.yorku.ca>
2020-06-26 14:05:38 -0400

    Update emergencHotline.js

--- frontend/mobile-app/screens/emergencHotline.js
+++ frontend/mobile-app/screens/emergencHotline.js
@@ -64,7 +64,7 @@ export default function EmergencyHotlinesScreen({ navigation }) {
 
                       call = () => {
                         const args = {
-                          number: '6475706640',
+                          number: '1234567890',
                           prompt: false,
                         };
                         call(args).catch(console.error);
