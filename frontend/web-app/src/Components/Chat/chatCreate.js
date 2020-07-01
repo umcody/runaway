2a8a2aa3310a8beea58d5822fd14e82665d36e14
Cody Um <yunsuum@gmail.com>
2020-07-01 11:09:06 +0900

    Basic send message feature

--- frontend/web-app/src/Components/Chat/chatCreate.js
+++ frontend/web-app/src/Components/Chat/chatCreate.js
@@ -1,36 +1,53 @@
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import socketioclient from "socket.io-client";
 
 
 /*
 THIS COMPONENT LIES UNDER MOBILE. IT IS CURRENTLY IN WEB JUST FOR DEV. PURPOSE
 */
-function ChatCreation(){
-    
-    const [queue, setQueue] = useState([]);
+let socket;
 
-    function socket_joinRoom (room){
-        return socketioclient("http://localhost:7000",{
-            query:"r_var="+room
-        });
+function ChatCreation() {
+
+    const [queue, setQueue] = useState([]);
+    const [newMessage, setNewMessage] = useState("");
+    const [roomNum, setRoomNum] = useState(0);
+
+    function socket_joinRoom(room) {
+        setRoomNum(room);
+        socket.emit("joinRoom",room);
+        socket.emit("pushQueue",room);
     }
 
 
-    useEffect(()=>{
+    useEffect(() => {
+        socket = socketioclient("http://localhost:7000");
         let random_room = Math.floor((Math.random() * 10) + 1);
-        //connect
-        let socket = socket_joinRoom(random_room);
-        //emit to the server the room num the user joined
-        socket.emit("pushQueue", random_room);
-        console.log(socket);
+        //join room
+        socket_joinRoom(random_room);
 
-        return() => socket.disconnect();
+        socket.on("updateMessage", function (message) {
+            console.log("message recieved");
+            setNewMessage(message);
+        })
+        //when exiting the component
+        return () => {
+            socket.disconnect();
+        }
     },[])
-    
 
-    
-    return(
-        <div>{`hello`}</div>
+    function sendMessage(event) {
+        socket.emit("sendMessage", event.target.value);
+    }
+
+
+    return (
+        <div>
+            <div>{roomNum}</div>
+            <div>{`hello`}</div>
+            <input onClick={sendMessage}></input>
+            <div>{newMessage}</div>
+        </div>
     )
 
 }
