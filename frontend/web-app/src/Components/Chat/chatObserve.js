6b512dea6e0f42d053f811976f444f9204db15fb
Cody Um <yunsuum@gmail.com>
2020-07-01 11:48:39 +0900

    Volunteer can now see message with the users

--- frontend/web-app/src/Components/Chat/chatObserve.js
+++ frontend/web-app/src/Components/Chat/chatObserve.js
@@ -1,30 +1,54 @@
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import socketioclient from "socket.io-client";
+let socket;
+function ChatObservation() {
 
-function ChatObservation(){
-    
     const [queue, setQueue] = useState([0]);
+    const [roomNum, setRoomNum] = useState(0);
+    const [newMessage, setNewMessage] = useState("");
 
-    
-    let socket;
 
-    useEffect(()=>{
+
+    function socket_joinRoom(room) {
+        setRoomNum(room);
+        socket.emit("joinRoom", room);
+    }
+
+    useEffect(() => {
         socket = socketioclient("http://localhost:7000");
-        socket.emit("observeQueue","joining General");
+        socket.emit("observeQueue", "joining General");
+
         socket.on("updateQueue", queue => {
             console.log(queue);
             setQueue(queue);
         });
-    },[])
+        socket.on("updateMessage", function (message) {
+            console.log("message recieved");
+            setNewMessage(message);
+        })
+    }, [])
 
-    return(
-        <div>{queue.map((room)=>{
-            return(
-                <div>
-                    {room}
-                </div>
-            )
-        })}</div>
+    function joinRoom(event) {
+        socket_joinRoom(event.target.innerHTML);
+        console.log(event.target)
+    }
+
+    return (
+        <div>
+            <h3>Chat Room Queues ( click to join )</h3>
+            <div>{queue.map((room) => {
+                return (
+                    <div onClick={joinRoom}>
+                        {room}
+                    </div>
+                )
+            })}</div>
+
+            <h3>Most Recent Messages</h3>
+            <div>{newMessage}</div>
+        </div>
+        
+
     )
 
 }
