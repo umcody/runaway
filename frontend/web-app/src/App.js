a9a6610998700bb8181be92ef3601dd305e3cee6
Cody Um <yunsuum@gmail.com>
2020-07-20 21:07:01 +0900

    dynamic redirection depending on logged in user's access level

--- frontend/web-app/src/App.js
+++ frontend/web-app/src/App.js
@@ -4,7 +4,6 @@ import BlogEditor from './Components/Blog/blogEditor';
 
 import Register from './Components/Auth/register';
 import Login from './Components/Auth/login';
-import ChatCreation from './Components/Chat/chatCreate';
 import Resource from './Components/Resource/resource';
 import Admin from "./Components/Admin/admin.js"
 import ChatObservation from './Components/Chat/chatObserve';
@@ -20,10 +19,12 @@ function App() {
       <link rel="stylesheet" href="//cdn.quilljs.com/1.2.6/quill.snow.css"></link>
       <Route exact path = "/login" component = {Login}/>
       <Route exact path = "/register" component = {Register}/>
-      <Route exact path = "/chat/oberve" component = {ChatObservation}/>
+      <Route exact path = "/chat/observe" component = {ChatObservation}/>
       <Route exact path = "/blog/write" component = {BlogEditor}/>
       <Route exact path = "/resource/write" component = {Resource}/>
       <Route exact path = "/admin/overview" component = {Admin}/>
+      <Route exact path = "/" component = {Login}/>
+
 
 
 
