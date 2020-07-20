0b1d2d14f544171a1e858a8149ec8aba2e58f2ad
Cody Um <yunsuum@gmail.com>
2020-07-20 20:39:53 +0900

    added admin and resource pages to the web app

--- frontend/web-app/src/App.js
+++ frontend/web-app/src/App.js
@@ -5,6 +5,8 @@ import BlogEditor from './Components/Blog/blogEditor';
 import Register from './Components/Auth/register';
 import Login from './Components/Auth/login';
 import ChatCreation from './Components/Chat/chatCreate';
+import Resource from './Components/Resource/resource';
+import Admin from "./Components/Admin/admin.js"
 import ChatObservation from './Components/Chat/chatObserve';
 
 
@@ -17,10 +19,13 @@ function App() {
       <div style = {{"color":"fffff","font-size":"60px","font-weight":"700"}}>RunAway</div>
       <link rel="stylesheet" href="//cdn.quilljs.com/1.2.6/quill.snow.css"></link>
       <Route exact path = "/login" component = {Login}/>
-    <Route exact path = "/register" component = {Register}/>
-      <Route exact path = "/chatCreate" component = {ChatCreation}/>
-      <Route exact path = "/chatObserve" component = {ChatObservation}/>
-      <Route exact path = "/" component = {BlogEditor}/>
+      <Route exact path = "/register" component = {Register}/>
+      <Route exact path = "/chat/oberve" component = {ChatObservation}/>
+      <Route exact path = "/blog/write" component = {BlogEditor}/>
+      <Route exact path = "/resource/write" component = {Resource}/>
+      <Route exact path = "/admin/overview" component = {Admin}/>
+
+
 
     </div>
     );
