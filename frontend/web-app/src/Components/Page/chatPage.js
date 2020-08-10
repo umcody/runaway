ce0f4bac1f309eb3c572199e6741cc98575b7630
Cody Um <yunsuum@gmail.com>
2020-08-10 18:21:20 -0500

    added auth? auth

--- frontend/web-app/src/Components/Page/chatPage.js
+++ frontend/web-app/src/Components/Page/chatPage.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NavBar from "../NavBar/NavBar";
 import Chat from "../Chat/chatQueue";
+import check from"../Auth/check";
 
 
-export default function blogPage(props){
+export default function ChatPage(props){
+    useEffect(async ()=>{
+        if(!(await check("volunteer"))){
+            window.location = "/login";
+        }
+    });
     return(
         <div style = {{position: "absolute",width:"100%", height:"100%"}} className="con container row">
                 <NavBar pages={["Dashboard", "Chat Room", "Resources"]}/>
