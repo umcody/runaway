601355ff6a30018b948ab68d85474683907ee635
cedricblaise <cblaise@bowdoin.edu>
2020-08-10 19:16:41 -0400

    Blog Pagination for Web App

--- frontend/web-app/src/App.js
+++ frontend/web-app/src/App.js
@@ -1,35 +1,34 @@
-import React from 'react';
-import {Route} from 'react-router-dom';
-import BlogPage from './Components/Page/blogPage';
+import React from "react";
+import { Route } from "react-router-dom";
+import BlogPage from "./Components/Page/blogPage";
+import Blogs from "./Components/Blog/blogs";
+import Register from "./Components/Auth/register";
+import Login from "./Components/Auth/login";
+import Resource from "./Components/Resource/resource";
+import Admin from "./Components/Admin/admin.js";
+import ChatObservation from "./Components/Chat/chatObserve";
 
-import Register from './Components/Auth/register';
-import Login from './Components/Auth/login';
-import Resource from './Components/Resource/resource';
-import Admin from "./Components/Admin/admin.js"
-import ChatObservation from './Components/Chat/chatObserve';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <link rel="stylesheet" href="//cdn.quilljs.com/1.2.6/quill.snow.css"></link>
-      <Route exact path = "/login" component = {Login}/>
-      <Route exact path = "/register" component = {Register}/>
-      <Route exact path = "/chat/observe" component = {ChatObservation}/>
-      <Route exact path = "/blog/write" component = {BlogPage}/>
-      <Route exact path = "/resource/write" component = {Resource}/>
-      <Route exact path = "/admin/overview" component = {Admin}/>
-      <Route exact path = "/" component = {Login}/>
-
-
-
-
+      <link
+        rel="stylesheet"
+        href="//cdn.quilljs.com/1.2.6/quill.snow.css"
+      ></link>
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+      <Route exact path="/chat/observe" component={ChatObservation} />
+      <Route exact path="/blog/write" component={BlogPage} />
+      <Route exact path="/blogs" component={Blogs} />
+      <Route exact path="/resource/write" component={Resource} />
+      <Route exact path="/admin/overview" component={Admin} />
+      <Route exact path="/" component={Login} />
     </div>
-    );
+  );
 }
 
 export default App;
---
+++ frontend/web-app/src/Components/Blog/blogs.js
@@ -0,0 +1,62 @@
+import React from "react";
+import axios from "axios";
+import ReactPaginate from 'react-paginate';
+import '../../styles/blogs.css'
+import moment from "moment";
+
+class Blogs extends React.Component {
+  state = {
+      blogs: [], 
+      total: null, 
+      currentPage: 1
+  };
+
+  componentDidMount() {
+    this.handlePageClick(1);
+  }
+
+  handlePageClick = page => {
+    axios.get(`http://localhost:3000/api/volunteer/blog/get/${page}`)
+        .then((response) => {
+            console.log(response.data);
+            this.setState({
+                blogs: response.data.blogs,
+                total: response.data.totalBlogs,
+                currentPage: response.data.page
+            })
+            console.log(this.state.total);
+        }) 
+    }
+
+  render() {
+    const pageCount = Math.ceil(this.state.total/20);
+
+    return (
+        <div>
+              { this.state.blogs.map(blog => (
+                  <div key={blog._id} style={{ textAlign: "left", margin: "10px", border: "2px solid black" }}> 
+                    <div style={{marginLeft: "10px"}}> 
+                        <h1>{blog.title}</h1>
+                        <h4>{blog.author}</h4>
+                        <h6>{moment(blog.date).format("MMM DD Y ")}</h6>
+                        <img src={blog.imageURL} alt="img" style={{ height: "300px", width: "450px"}}/>
+                        <p dangerouslySetInnerHTML={{ __html: blog.content }}/>
+                    </div>
+                </div>
+              )) }
+        
+        <ReactPaginate
+                    pageCount={pageCount}
+                    marginPagesDisplayed={2}
+                    pageRangeDisplayed={5}
+                    onPageChange={(e) => this.handlePageClick(e.selected)}
+                    containerClassName={"pagination"}
+                    subContainerClassName={"pages pagination"}
+                    activeClassName={"active"}/>
+
+      </div>
+    );
+  }
+}
+
+export default Blogs;
