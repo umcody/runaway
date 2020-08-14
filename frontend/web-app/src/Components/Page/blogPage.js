5292fac26a945e5d3a8a7eb8f456724bab8cca7d
Cody (Yun Su) Um <yunsuum@gmail.com>
2020-08-14 17:48:20 -0500

    Revert "when user exits the room, the room terminates, and volunteers can clo…"

--- frontend/web-app/src/Components/Page/blogPage.js
+++ frontend/web-app/src/Components/Page/blogPage.js
@@ -1,15 +1,14 @@
-import React, { useEffect } from "react";
-import { Route } from "react-router-dom";
+import React,{useEffect} from "react";
+import {Route} from "react-router-dom";
 import NavBar from "../NavBar/NavBar";
 import BlogEditor from "../Blog/blogEditor";
 import AllBlogs from "../Blog/blogs";
 import check from "../Auth/check";
-import AccountBar from "../AccountBar/AccountBar";
 
 
 
 
-export default function BlogPage(props) {
+export default function BlogPage(props){
     useEffect(() => {
         async function checkAccess() {
             if (!(await check("blog editor"))) {
@@ -17,16 +16,14 @@ export default function BlogPage(props) {
             }
         }
         checkAccess();
-    }, [])
+    },[])
 
-    return (
-        <div style={{ position: "absolute", width: "100%", height: "100%" }} className="con container row">
-            <NavBar pages={["All Posts", "Your Posts", "Drafts"]} />
-            <div className = "col-10">
-                    <AccountBar />
-                    <Route path="/blog/yourposts" component={BlogEditor} />
-                    <Route path="/blog/allposts" component={AllBlogs} />
-            </div>
+    return(
+        <div style = {{position: "absolute",width:"100%", height:"100%"}} className="con container row">
+            <NavBar pages={["All Posts", "Your Posts", "Drafts"]}/>
+            <Route path="/blog/yourposts" component={BlogEditor}/>
+            <Route path="/blog/allposts" component={AllBlogs}/>
+                
         </div>
     )
 }
