310e410024f3f56f28ca9b235496ce2c623ada20
Yasir Azizi <yasir720@my.yorku.ca>
2020-08-14 18:02:28 -0400

    still have some bugs and styling to fix

--- frontend/mobile-app/components/HotlineCard.js
+++ frontend/mobile-app/components/HotlineCard.js
@@ -1,128 +1,100 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Dimensions,Linking, Modal, TouchableHighlight,Alert, Platform } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import {colors, fonts, padding, dimensions,margin,borderRadius, icon} from '../style/styleValues.js'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  Linking,
+  Modal,
+  TouchableHighlight,
+  Alert,
+  Platform,
+} from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import {
+  colors,
+  fonts,
+  padding,
+  dimensions,
+  margin,
+  borderRadius,
+  icon,
+} from "../style/styleValues.js";
 import { Feather, MaterialCommunityIcons } from "@expo/vector-icons";
-
-
-
-
-
-
-
+import sms from "react-native-sms-linking";
 import * as WebBrowser from "expo-web-browser";
-function getSMSDivider(): string {
-    return Platform.OS === "ios" ? "&" : "?";
-  }
 
-const HotlineCard = ({item}) => {
-    const [modalVisible, setModalVisible] = useState(false);
-    return (
-        // <View style={styles.container}>
-        //     <TouchableOpacity
-        //         style={styles.cardView}
-        //         onPress={() => {
-        //             WebBrowser.openBrowserAsync("https://www." +item.website)
-        //           }}>
-        //         <Text style={styles.title}>{item.title}</Text>
-        //         <Text style={styles.title}>{item.phoneNumber}</Text>
-        //         <Text style={styles.title}>{item.textNumber}</Text>
-        //         <Text style={styles.title}>{item.firstText}</Text>              
-        //         <Text style={styles.title}>{item.website}</Text>
-        //         <Text style={styles.title}>{item.meantFor}</Text>
-        //     </TouchableOpacity>           
-        // </View>
-
-
-
-        <View style={styles.centeredView}>      
-        <Modal
-          animationType="slide"
-          transparent={true}
-          visible={modalVisible}
-          onRequestClose={() => {this.setModalVisible(false)}}
-        >
-          <View style={styles.centeredView}>
-          
-            <View style={styles.modalView}>
-            <View style={{alignSelf: "flex-end"}}>
-                <TouchableOpacity 
+const HotlineCard = ({ item }) => {
+  const [modalVisible, setModalVisible] = useState(false);
+  return (
+    <View style={styles.centeredView}>
+      <Modal animationType="slide" transparent={true} visible={modalVisible}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <View style={{ alignSelf: "flex-end" }}>
+              <TouchableHighlight
                 onPress={() => {
-                    setModalVisible(!modalVisible);}}>
-                    <Feather name="x" size={60} color="red" />
-                </TouchableOpacity>
+                  setModalVisible(!modalVisible);
+                }}
+              >
+                <Feather name="x" size={60} color="red" />
+              </TouchableHighlight>
             </View>
-              <Text style={styles.modalText}>Name</Text>
-  
-  
-  
-              <View style={styles.modalButtons}>
-                
+            <Text style={styles.modalText}>Name</Text>
+
+            <View style={styles.modalButtons}>
               <TouchableHighlight
                 style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
                 onPress={() => {
-                    Linking.openURL(`tel:${item.phoneNumber}`)
+                  Linking.openURL(`tel:${item.phoneNumber}`);
                 }}
               >
-                
                 <View style={styles.modalButtons2}>
                   <Feather name="phone-call" size={30} color="white" />
                   <Text style={styles.textStyle}>Call</Text>
-                  
                 </View>
-                
               </TouchableHighlight>
-  
+
               <TouchableHighlight
                 style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
                 onPress={() => {
-                    Linking.openUrl(`sms:${item.textNumber}?sms_body=${item.firstText}`)
+                  sms(String(item.textNumber), String(item.firstText));
                 }}
               >
-                
                 <View style={styles.modalButtons2}>
                   <Feather name="message-square" size={30} color="white" />
                   <Text style={styles.textStyle}>Text</Text>
-                  
                 </View>
-            
               </TouchableHighlight>
-  
+
               <TouchableHighlight
                 style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
                 onPress={() => {
-                    WebBrowser.openBrowserAsync(item.website)
+                  WebBrowser.openBrowserAsync(item.website);
                 }}
               >
-                
                 <View style={styles.modalButtons2}>
                   <MaterialCommunityIcons name="web" size={30} color="white" />
                   <Text style={styles.textStyle}>Visit Site</Text>
-                  
                 </View>
-                
-  
               </TouchableHighlight>
-              </View>
-  
-              
             </View>
           </View>
-        </Modal>
-  
-        <TouchableOpacity
-          style={styles.openButton}
-          onPress={() => {
-            setModalVisible(true);
-          }}
-        >
-          <Text style={styles.textStyle}>{item.title}</Text>
-          <Text style={styles.textStyle}>{item.meantFor}</Text>
-        </TouchableOpacity>
-      </View>
-    )
-}
+        </View>
+      </Modal>
 
+      <TouchableOpacity
+        style={styles.openButton}
+        onPress={() => {
+          setModalVisible(true);
+        }}
+      >
+        <Text style={styles.textStyle}>{item.title}</Text>
+        <Text style={styles.textStyle}>{item.meantFor}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 // <View>
 //     <Modal
@@ -133,35 +105,14 @@ const HotlineCard = ({item}) => {
 //           Alert.alert("Modal has been closed.");
 //         }}
 //       >
-          
+
 //       </Modal>
 
 // </View>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // const styles = StyleSheet.create({
 //     container: {
-//         backgroundColor: colors.background,        
+//         backgroundColor: colors.background,
 //         alignItems: 'center',
 //         flex:1,
 //     },
@@ -191,56 +142,51 @@ const HotlineCard = ({item}) => {
 //     },
 //   });
 
-
-
-
-
 const styles = StyleSheet.create({
-    centeredView: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-      marginTop: 22,
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 15,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    modalView: {
-      margin: 20,
-      backgroundColor: "white",
-      borderRadius: 20,
-      padding: 15,
-      alignItems: "center",
-      shadowColor: "#000",
-      shadowOffset: {
-        width: 0,
-        height: 2
-      },
-      shadowOpacity: 0.25,
-      shadowRadius: 3.84,
-      elevation: 5,
-  
-    },
-    modalButtons: {
-      flexDirection: "row",
-    },
-    modalButtons2: {
-      alignItems: "center",
-    },
-    openButton: {
-      backgroundColor: "#F194FF",
-      borderRadius: 20,
-      padding: 10,
-      elevation: 2
-    },
-    textStyle: {
-      color: "white",
-      fontWeight: "bold",
-      textAlign: "center"
-    },
-    modalText: {
-      fontSize: 24,
-      marginBottom: 10,
-      textAlign: "center",
-      color: "pink",
-    }
-  });
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  modalButtons: {
+    flexDirection: "row",
+  },
+  modalButtons2: {
+    alignItems: "center",
+  },
+  openButton: {
+    backgroundColor: "#F194FF",
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    fontSize: 24,
+    marginBottom: 10,
+    textAlign: "center",
+    color: "pink",
+  },
+});
 
-export default HotlineCard
+export default HotlineCard;
